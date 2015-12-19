9a81791dc77a9891ba8ae172fcde1b7f6f4da206
PhazeIllusions <league@WTS15.attlocal.net>
2015-12-19 11:48:22 -0800

    Fortune Cookie
    Fortune Cookie

---
+++ Lv-1/src/Fortune_Cookie.java
@@ -0,0 +1,70 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Fortune_Cookie implements ActionListener {
+
+	private JFrame Frame;
+	private JButton Button;
+	private JPanel Panel;
+
+	public Fortune_Cookie() {
+		Frame = new JFrame();
+		Panel = new JPanel();
+		Button = new JButton();
+		Frame.setVisible(true);
+		Frame.setSize(100, 100);
+		int rand = new Random().nextInt(5);
+
+		Frame.add(Panel);
+		Panel.add(Button);
+		Button.addActionListener(this);
+	}
+
+	public static void main(String[] args) {
+		Fortune_Cookie h = new Fortune_Cookie();
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method random = Integer.parseInt(rand);
+		int rand = new Random().nextInt(6);
+System.out.println(rand);
+		if (rand == 0) {
+			JOptionPane.showMessageDialog(null, "About time you got this out of a fortune cookie");
+
+		}
+		if (rand == 1) {
+			JOptionPane.showMessageDialog(null, "Wouldn't it be ironic to die in the living room?");
+
+		}
+		if (rand == 2) {
+			JOptionPane.showMessageDialog(null, "The fortune you seek will be in another cookie");
+
+		}
+
+		if (rand == 3) {
+			JOptionPane.showMessageDialog(null, "You will be hungry again in one hour");
+
+		}
+		if (rand == 4) {
+			JOptionPane.showMessageDialog(null, "Meh, i'm out of 'em");
+
+		}
+		if (rand == 5) {
+			JOptionPane.showMessageDialog(null, "People rely on your dependability..... In bed!");
+
+		}
+
+	}
+
+	void showButton() {
+
+	}
+}
---
+++ Lv-1/src/Fortune_Cookie_Instructions.java
@@ -0,0 +1,25 @@
+// Make a class, main method and showButton method. Put a printout in the showButton method and challenge kids to get it
+// printing (they will need to instantiate class and call method on the instance).
+// FortuneCookie fortune = new FortuneCookie();
+// fortune.showButton();
+//
+// Make a JFrame in showButton method and get it to show.
+// jFrame.setVisible(true);
+//
+// Make a JButton and add it to the JFrame.
+// JButton butt = new JButton();
+// jFrame.add(butt);
+//
+// When the button is clicked, say “Woohoo”.
+// Add an action listener to the button:jButton.addActionListener(this);
+// Let your class implement ActionListener [use quick fix]
+// Add unimplemented methods [use quick fix]
+// Put JOptionPane pop-up inside actionPerformed() method
+//
+// Generate a random number between 0 and 4:
+// int rand = new Random().nextInt(5);
+//
+// Depending on which random number, show one of the five fortunes.
+//
+// Quiz kids (using easy buttons as buzzers) on all of this.
+//
